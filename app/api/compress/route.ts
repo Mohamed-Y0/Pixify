b2e8ce8e06f74b6103fc5febf2a2a6b1ee0b69de
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-10-18 02:18:43 +0300

    add try catch for better error handling

--- app/api/compress/route.ts
+++ app/api/compress/route.ts
@@ -1,23 +1,34 @@
-import { compressImage } from "@/lib/compressImage";
+import { compressImage } from "@/utils/compressImage";
 import { NextResponse } from "next/server";
 import sharp from "sharp";
 
 export async function POST(request: Request) {
-  const formData = await request.formData();
-  const file = formData.get("file") as File;
-  const quality = formData.get("quality");
+  try {
+    const formData = await request.formData();
+    const file = formData.get("file") as File;
+    const quality = formData.get("quality");
 
-  if (!file) return NextResponse.json({ error: "No file" }, { status: 400 });
+    if (!file) return NextResponse.json({ error: "No file" }, { status: 400 });
 
-  const arrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  const image = sharp(buffer);
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const image = sharp(buffer);
 
-  const imageFormat = (await image.metadata()).format || "jpeg";
+    const imageFormat = (await image.metadata()).format || "jpeg";
 
-  const compressed = await compressImage(buffer, imageFormat, Number(quality));
+    const compressed = await compressImage(
+      buffer,
+      imageFormat,
+      Number(quality),
+    );
 
-  return new NextResponse(new Uint8Array(compressed), {
-    headers: { "Content-Type": `image/${imageFormat}` },
-  });
+    return new NextResponse(new Uint8Array(compressed), {
+      headers: { "Content-Type": `image/${imageFormat}` },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Server error: ${(error as Error).message}` },
+      { status: 500 },
+    );
+  }
 }
