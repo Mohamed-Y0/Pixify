855b683152f5d768be24f80dc001bebc31b3a91e
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-10-30 03:27:42 +0300

    add the size of the image

--- components/UploadedPreview.tsx
+++ components/UploadedPreview.tsx
@@ -1,3 +1,4 @@
+import { formatFileSize } from "@/utils/formatFileSize";
 import { X } from "lucide-react";
 import Image from "next/image";
 
@@ -5,10 +6,12 @@ type Props = {
   id: string;
   imageUrl: string;
   fileName: string;
+  size: number;
   onRemove: (id: string) => void;
 };
 
-function UploadedPreview({ id, imageUrl, fileName, onRemove }: Props) {
+function UploadedPreview({ id, imageUrl, fileName, size, onRemove }: Props) {
+  console.log(size);
   return (
     <div className="bg-gray3 flex w-full items-center justify-between gap-2.5 overflow-hidden rounded-lg pr-2">
       <div className="flex h-15 items-center gap-2.5">
@@ -19,7 +22,10 @@ function UploadedPreview({ id, imageUrl, fileName, onRemove }: Props) {
           alt={imageUrl}
           className="rounded-sm"
         />
-        <p className="text-xs">{fileName}</p>
+        <div className="flex flex-col">
+          <p className="text-sm">{fileName}</p>
+          <span className="text-[10px]">{formatFileSize(size)}</span>
+        </div>
       </div>
 
       <div>
