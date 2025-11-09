56f4bb68f1f6ff3c87dc0ae952fdb6e965254ed7
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-11-09 07:02:58 +0200

    write some comments in route.ts

--- app/api/compress/route.ts
+++ app/api/compress/route.ts
@@ -4,6 +4,7 @@ import sharp from "sharp";
 
 export async function POST(request: Request) {
   try {
+    // get the data from formData
     const formData = await request.formData();
     const file = formData.get("file") as File;
     const quality = formData.get("quality") || 75;
@@ -14,15 +15,26 @@ export async function POST(request: Request) {
     const buffer = Buffer.from(arrayBuffer);
 
     const image = sharp(buffer);
+    // in case the image is .jpg because sharp doesn't support .jpg type
     const imageFormat = (await image.metadata()).format || "jpeg";
 
+    // Compress the image to the same format (change format is not available now)
     const compressed = await image
       .toFormat(imageFormat, {
+        // Change 'quality' from string to number (all data in the formData is string)
+        // This line work in case the format is not .png
         quality: Number(quality),
+
+        // compressionLevel is for .png
+        // We send the quality to helper function because compressionLevel is from 0 to 9
+        // Side note: the default compressionLevel is 6 because it's more commonly use - According to sharp docs (v0.28.0)
         compressionLevel:
           imageFormat === "png"
             ? qualityToPngLevel(Number(quality))
             : undefined,
+
+        // This option for .jpeg images - it's to reduce file size while maintaining quality
+        // !! May increase processing time !!
         mozjpeg: true,
       })
       .toBuffer();
