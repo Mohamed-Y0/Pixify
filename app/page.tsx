2291f89466b5c0d817bdae061ff3b5ccac8de151
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-11-11 09:58:46 +0200

    improve styling: add nice animation

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,21 @@ export default function Home() {
   return (
     <>
       <section className="flex flex-col gap-10 px-2.5 md:px-0">
-        <div className="flex flex-col items-center pt-8 text-center md:pt-16">
+        <div className="animate-fade-in-from-right-1 flex flex-col items-center pt-8 text-center md:pt-16">
           <h1 className="max-w-xl py-6 text-2xl font-semibold md:text-5xl">
-            Make image optimization & effortless{" "}
-            <span className={`font-telma`}> with Pixify</span>
+            Make image optimization & effortless
+            <span className={`font-telma animate-remove-blur`}>
+              {" "}
+              with Pixify
+            </span>
           </h1>
           <p className="max-w-sm py-2.5 text-sm tracking-wider text-gray-500 md:text-lg">
             Compress your files, change formats, and keep stunning quality.
           </p>
         </div>
-        <UploadingCard />
+        <div className="animate-fade-in-from-right-2">
+          <UploadingCard />
+        </div>
       </section>
     </>
   );
