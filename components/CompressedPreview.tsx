9655675b16955d715699a6d429158ad6eecf2cf9
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-11-15 06:59:22 +0200

    add: download button

--- components/CompressedPreview.tsx
+++ components/CompressedPreview.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { FileInfo } from "@/types/uploadingTypes";
 import { formatFileSize, getFileName } from "@/utils/helpers";
+import { LucideDownload } from "lucide-react";
 import Link from "next/link";
 
 type Props = {
@@ -13,18 +15,21 @@ function CompressedPreview({ url, file }: Props) {
   return (
     <>
       <Separator />
+      <div className="flex items-center justify-between">
+        <div className="flex max-h-15 w-1/2 items-center gap-2.5 overflow-hidden rounded-lg border py-5">
+          <img height={100} width={100} src={url} alt="Compressed result" />
 
-      <div className="flex max-h-15 w-1/2 items-center gap-2.5 overflow-hidden rounded-lg border py-5">
-        <img height={100} width={100} src={url} alt="Compressed result" />
-
-        <div className="flex flex-col truncate">
-          <p className="text-sm">{getFileName(file.name)}</p>
-          <span className="text-[10px]">{formatFileSize(file.size)}</span>
+          <div className="flex flex-col truncate">
+            <p className="text-sm">{getFileName(file.name)}</p>
+            <span className="text-[10px]">{formatFileSize(file.size)}</span>
+          </div>
         </div>
 
-        <Link href={url} download={`${getFileName(file.name)}-pixify`}>
-          Download
-        </Link>
+        <Button asChild className="p-5">
+          <Link href={url} download={`${getFileName(file.name)}-pixify`}>
+            <LucideDownload className="size-5" />
+          </Link>
+        </Button>
       </div>
     </>
   );
