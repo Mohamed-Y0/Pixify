8550927572cb296816045f6440c5fcc76d8f8787
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-11-15 07:20:05 +0200

    remove unnecessary tailwind styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ export default function Home() {
   return (
     <>
       <section className="flex flex-col gap-10 px-2.5 pt-7 pb-10 md:px-0 md:pt-15">
-        <div className="animate-fade-in-from-right-1 flex flex-col items-center text-center">
-          <h1 className="py-6 text-3xl font-semibold sm:max-w-xl md:text-5xl">
+        <div className="animate-fade-in-from-right-1 flex flex-col items-center gap-2.5 text-center">
+          <h1 className="text-3xl font-semibold sm:max-w-xl md:text-5xl">
             Make image optimization & effortless
             <span className={`font-telma animate-remove-blur`}>
               {" "}
               with Pixify
             </span>
           </h1>
-          <p className="py-2.5 text-sm tracking-wider text-gray-500 sm:max-w-sm md:text-lg">
+          <p className="text-sm tracking-wider text-gray-500 sm:max-w-sm md:text-lg">
             Compress your files, change formats, and keep stunning quality.
           </p>
         </div>
